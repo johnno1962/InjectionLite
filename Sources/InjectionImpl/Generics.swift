013654bdd110208edbe448f4a208386cbde33af4
John Holdsworth <1786033+johnno1962@users.noreply.github.com>
2025-08-25 15:35:59 +0200

    Avoid ARC.

--- Sources/InjectionImpl/Generics.swift
+++ Sources/InjectionImpl/Generics.swift
@@ -16,23 +16,24 @@ import fishhookD
 import DLKitD
 #endif
 
+private typealias ClassMetaData = UnsafeRawPointer
 private struct TrackingGenerics {
     typealias GenericAllocFunc = @convention(c) (UnsafeMutableRawPointer,
-                        UnsafeRawPointer, UnsafeRawPointer) -> AnyClass
+                        UnsafeRawPointer, UnsafeRawPointer) -> ClassMetaData
 
     static let allocFuncName = "swift_allocateGenericClassMetadata"
     static var save_allocateGeneric: GenericAllocFunc!
 
     private static var registryLock = os_unfair_lock()
-    private static var _registry = [String: [AnyClass]]()
+    private static var _registry = [String: [ClassMetaData]]()
     
-    static func addToRegistry(baseName: String, newClass: AnyClass) {
+    static func addToRegistry(baseName: String, newClass: ClassMetaData) {
         os_unfair_lock_lock(&registryLock)
         defer { os_unfair_lock_unlock(&registryLock) }
         _registry[baseName, default: []].append(newClass)
     }
     
-    static func getClasses(for baseName: String) -> [AnyClass] {
+    static func getClasses(for baseName: String) -> [ClassMetaData] {
         os_unfair_lock_lock(&registryLock)
         defer { os_unfair_lock_unlock(&registryLock) }
         return _registry[baseName] ?? []
@@ -55,15 +56,16 @@ public func hookGenerics(original: UnsafeMutableRawPointer,
 }
 
 @_cdecl("injection_allocateGenericClassMetadata")
-public func injection_allocateGeneric(description: UnsafeMutableRawPointer,
-    arguments: UnsafeRawPointer, pattern: UnsafeMutableRawPointer) -> AnyClass {
-    let newClass: AnyClass = TrackingGenerics.save_allocateGeneric(description, arguments, pattern)
-    let fullName = _typeName(newClass)
+public func injection_allocateGenericClass(description: UnsafeMutableRawPointer,
+    arguments: UnsafeRawPointer, pattern: UnsafeMutableRawPointer) -> UnsafeRawPointer {
+    let typeMeta = TrackingGenerics
+        .save_allocateGeneric(description, arguments, pattern)
+    let fullName = _typeName(unsafeBitCast(typeMeta, to: Any.Type.self))
     if let params = fullName.firstIndex(of: "<") {
         let baseName = String(fullName.prefix(upTo: params))
-        TrackingGenerics.addToRegistry(baseName: baseName, newClass: newClass)
+        TrackingGenerics.addToRegistry(baseName: baseName, newClass: typeMeta)
     }
-    return newClass
+    return typeMeta
 }
 
 #if canImport(Nimble) || SWIFT_PACKAGE // InjectionNext
@@ -71,8 +73,9 @@ extension Sweeper {
     func hookedPatch(of generics: Set<String>, in image: ImageSymbols) -> [AnyClass] {
         var patched = Set<UnsafeRawPointer>()
         for baseName in generics {
-            for special in TrackingGenerics.getClasses(for: baseName) {
-                _ = patchGenerics(oldClass: special, image: image,
+            for typeMeta in TrackingGenerics.getClasses(for: baseName) {
+                let oldClass: AnyClass = unsafeBitCast(typeMeta, to: AnyClass.self)
+                _ = patchGenerics(oldClass: oldClass, image: image,
                                   injectedGenerics: generics, patched: &patched)
             }
         }
@@ -88,8 +91,9 @@ extension SwiftInjection {
     static func hookedPatch(of generics: Set<String>, tmpfile: String) -> [AnyClass] {
         var patched = Set<UnsafeRawPointer>()
         for baseName in generics {
-            for special in TrackingGenerics.getClasses(for: baseName) {
-                _ = patchGenerics(oldClass: special, tmpfile: tmpfile,
+            for typeMeta in TrackingGenerics.getClasses(for: baseName) {
+                let oldClass: AnyClass = unsafeBitCast(typeMeta, to: AnyClass.self)
+                _ = patchGenerics(oldClass: oldClass, tmpfile: tmpfile,
                                   injectedGenerics: generics, patched: &patched)
             }
         }
